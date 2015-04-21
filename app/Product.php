00899b150a73b04221b64bb26ca8d73c449e8b26
robin hood <fordarnold@gmail.com>
2015-04-21 06:04:00 +0300

    Product-Tag relationship set up

--- app/Product.php
+++ app/Product.php
@@ -4,6 +4,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model {
 
-	//
+	protected $table = "tags";
+
+	protected $perPage = 20;
+
+	protected $fillable = array(
+		'category_id', 'name', 'vendor', 'description', 'photo',
+		'unit_cost', 'unit_price', 'currency_id', 'min_stock'
+	);
+
+	/**
+	 * Relationships
+	 */
+	public function tags()
+	{
+		$this->belongsToMany('Tag');
+	}
 
 }
