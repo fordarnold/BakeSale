30f2eb3d58d74678df95f22ed8dc491a839cc6c9
robin hood <fordarnold@gmail.com>
2015-04-22 00:02:27 +0300

    Menu templates feature put on hold for now

--- app/Menu.php
+++ app/Menu.php
@@ -8,12 +8,17 @@ class Menu extends Model {
 
 	protected $perPage = 20;
 
-	protected $fillable = array('name', 'description', 'photo', 'template');
+	protected $fillable = array('name', 'description', 'photo', 'template_id');
 
 	/**
 	 * Relationships
 	 */
 	
+	// public function template()
+	// {
+	// 	$this->belongsTo('Template');
+	// }
+
 	public function products()
 	{
 		$this->belongsToMany('Product');
