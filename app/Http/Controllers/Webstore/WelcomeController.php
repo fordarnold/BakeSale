8a0e16ccba6b3f6c6158dc62a749ccf60efc22cf
robin hood <fordarnold@gmail.com>
2015-04-22 19:15:26 +0300

    routes /webstore, /backroom and /kremlin all working :)

--- app/Http/Controllers/Webstore/WelcomeController.php
+++ app/Http/Controllers/Webstore/WelcomeController.php
@@ -1,27 +1,8 @@
 <?php namespace Bakesale\Http\Controllers\Webstore;
 
-class WelcomeController extends Controller {
+use Bakesale\Http\Controllers\WebstoreController;
 
-	/*
-	|--------------------------------------------------------------------------
-	| Welcome Controller
-	|--------------------------------------------------------------------------
-	|
-	| This controller renders the "marketing page" for the application and
-	| is configured to only allow guests. Like most of the other sample
-	| controllers, you are free to modify or remove it as you desire.
-	|
-	*/
-
-	/**
-	 * Create a new controller instance.
-	 *
-	 * @return void
-	 */
-	public function __construct()
-	{
-		$this->middleware('guest');
-	}
+class WelcomeController extends WebstoreController {
 
 	/**
 	 * Show the application welcome screen to the user.
---
+++ resources/views/webstore/home.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.webstore')
+
+@section('content')
+<div class="row">
+	<div class="large-12 columns">
+		
+		<section class="webstore home">
+
+			<hgroup>
+				<h2>Welcome to The Webstore</h2>
+				<h4 class="subheader">
+					This area can be accessed by any user with login details. Enjoy.
+				</h4>
+				<hr>
+			</hgroup>
+
+		</section>
+
+	</div>
+</div>
+@endsection
