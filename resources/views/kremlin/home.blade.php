ab62d2f75e21ea552bc131d334c861b90c742370
robin hood <fordarnold@gmail.com>
2015-04-22 19:08:25 +0300

    Oops

--- resources/views/kremlin/home.blade.php
+++ resources/views/kremlin/home.blade.php
@@ -23,11 +23,11 @@
 					</p>
 
 					<dl>
-						<dt><a href="{{ url('backroom/modules') }}">Modules</a></dt>
+						<dt><a href="{{ url('kremlin/modules') }}">Modules</a></dt>
 						<dd>The enabled system modules. Different packages have different enabled modules.</dd>
 					</dl>
 					<dl>
-						<dt><a href="{{ url('backroom/users') }}">Users</a></dt>
+						<dt><a href="{{ url('kremlin/users') }}">Users</a></dt>
 						<dd>You are able create new user accounts, including system admin users.</dd>
 					</dl>
 				</article>
