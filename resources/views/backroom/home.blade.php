6ba9a1d4b29b75cbaafc09b82dff549befb912c2
robin hood <fordarnold@gmail.com>
2015-04-22 18:28:21 +0300

    Set up the backroom and the kremlin home pages

--- resources/views/backroom/home.blade.php
+++ resources/views/backroom/home.blade.php
@@ -14,20 +14,50 @@
 				<hr>
 			</hgroup>
 			
-			<div class="panel large-6 columns">
+			<div class="panel radius">
 				<article>
 					<h5>What resources are available?</h5>
 
-					<ul>
-						<li><a href="{{ url('backroom/categories') }}">Categories</a></li>
-						<li><a href="{{ url('backroom/products') }}">Products</a></li>
-						<li><a href="{{ url('backroom/tags') }}">Tags</a></li>
-						<li><a href="{{ url('backroom/menus') }}">Menus</a></li>
-						<li><a href="{{ url('backroom/sales') }}">Sales</a></li>
-						<li><a href="{{ url('backroom/purchases') }}">Purchases</a></li>
-						<li><a href="{{ url('backroom/categories') }}">Categories</a></li>
-						<li><a href="{{ url('backroom/categories') }}">Categories</a></li>
-					</ul>
+					<dl>
+						<dt><a href="{{ url('backroom/categories') }}">Categories</a></dt>
+						<dd>Categories are broad groupings of products.</dd>
+					</dl>
+					<dl>
+						<dt><a href="{{ url('backroom/tags') }}">Tags</a></dt>
+						<dd>Tags are words or phrases which describe the specific details of a product.</dd>
+					</dl>
+					<dl>
+						<dt><a href="{{ url('backroom/menus') }}">Menus</a></dt>
+						<dd>Products can be added to menus (just like restaurant menus).</dd>
+					</dl>
+					<dl>
+						<dt><a href="{{ url('backroom/products') }}">Products</a></dt>
+						<dd>Arguably the main feature of an ecommerce application.</dd>
+					</dl>
+					<dl>
+						<dt><a href="{{ url('backroom/stocks') }}">Product Stocks</a></dt>
+						<dd>How much quantity of a product is available.</dd>
+					</dl>
+					<dl>
+						<dt><a href="{{ url('backroom/sales') }}">Sales</a></dt>
+						<dd>Orders made by customers to acquire products.</dd>
+					</dl>
+					<dl>
+						<dt><a href="{{ url('backroom/purchases') }}">Purchases</a></dt>
+						<dd>Orders made by your business to suppliers for purchase of different products.</dd>
+					</dl>
+					<dl>
+						<dt><a href="{{ url('backroom/customers') }}">Customers</a></dt>
+						<dd>The people or companies that buy products from your business.</dd>
+					</dl>
+					<dl>
+						<dt><a href="{{ url('backroom/suppliers') }}">Suppliers</a></dt>
+						<dd>The people or companies that sell products to your business.</dd>
+					</dl>
+					<dl>
+						<dt><a href="{{ url('backroom/users') }}">Users</a></dt>
+						<dd>You are able create new user accounts.</dd>
+					</dl>
 				</article>
 			</div>
 
